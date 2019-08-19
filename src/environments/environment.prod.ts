c2d06391d8b4a7f087e21038cd7fb491b240f40d
srinivasan v <svaikund@yahoo.com>
2019-08-19 20:50:05 +0530

    updated service urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  ep1: 'http://a4558a1e1c22a11e9ba480a702d2e678-1272775375.ap-south-1.elb.amazonaws.com:8080',
-  ep2: 'http://a4558a1e1c22a11e9ba480a702d2e678-1272775375.ap-south-1.elb.amazonaws.com:8080',
-  ep3: 'http://a4558a1e1c22a11e9ba480a702d2e678-1272775375.ap-south-1.elb.amazonaws.com:8080'
+  ep1: 'http://acbc85f6ac29211e9ba480a702d2e678-1956447560.ap-south-1.elb.amazonaws.com:8080',
+  ep2: 'http://acbc85f6ac29211e9ba480a702d2e678-1956447560.ap-south-1.elb.amazonaws.com:8080',
+  ep3: 'http://acbc85f6ac29211e9ba480a702d2e678-1956447560.ap-south-1.elb.amazonaws.com:8080'
 };
 
 
