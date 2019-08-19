fd4b0b781a025fa1cca2abcc389d04e59023dc6d
srinivasan v <svaikund@yahoo.com>
2019-08-19 09:10:41 +0530

    updated backend services links

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  ep1: 'http://localhost:8080',
-  ep2: 'http://localhost:8090',
-  ep3: 'http://localhost:8070'
+  ep1: 'http://a4558a1e1c22a11e9ba480a702d2e678-1272775375.ap-south-1.elb.amazonaws.com:8080',
+  ep2: 'http://a4558a1e1c22a11e9ba480a702d2e678-1272775375.ap-south-1.elb.amazonaws.com:8080',
+  ep3: 'http://a4558a1e1c22a11e9ba480a702d2e678-1272775375.ap-south-1.elb.amazonaws.com:8080'
 };
 
 
